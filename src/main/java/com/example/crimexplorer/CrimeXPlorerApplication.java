175f97939c5f9d06ea0d8a6a5f7c21cd4f9aa8a2
aramani9 <aramani9@asu.edu>
2023-11-25 16:16:58 -0700

    Initial commit

---
+++ src/main/java/com/example/crimexplorer/CrimeXPlorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.crimexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeXPlorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrimeXPlorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crimexplorer/CrimeXPlorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crimexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeXPlorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
