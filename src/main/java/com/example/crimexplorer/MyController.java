38eb86e09b498420083e36019affcc684bff41b9
Karthik Vaida <jvaida@asu.edu>
2023-12-03 22:43:25 -0700

    added 2 APIs

--- src/main/java/com/example/crimexplorer/MyController.java
+++ src/main/java/com/example/crimexplorer/MyController.java
@@ -25,15 +25,32 @@ public class MyController {
     @Value("${fuseki.server.password}")
     private String fusekiPassword;
 
-    @GetMapping("/getCrimes/{state}")
+
+    @GetMapping("/getAllCrimes")
+    public String getAllCrimes() {
+        String sparqlQuery = buildSparqlQuery(null);
+        return executeSparqlQuery(sparqlQuery);
+    }
+    @GetMapping("/getCrimesByState/{state}")
     public String getCrimesByState(@PathVariable String state) {
         String sparqlQuery = buildSparqlQuery(state);
         return executeSparqlQuery(sparqlQuery);
     }
 
     private String buildSparqlQuery(String state) {
-        // SPARQL query with the state variable incorporated
-        String query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX ex: <http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#> SELECT (REPLACE(str(?crimeId), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?crimeIdStripped) (REPLACE(str(?county), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?countyStripped) (REPLACE(str(?state), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?stateStripped) (REPLACE(str(?latitude), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?latitudeStripped) (REPLACE(str(?longitude), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?longitudeStripped) ?date (REPLACE(str(?type), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?typeStripped) ?weapon WHERE { ?crimeId rdf:type owl:NamedIndividual. ?crimeId ex:hasCounty ?county . ?crimeId ex:hasState ?state . ?crimeId ex:hasLatitude ?latitude . ?crimeId ex:hasLongitude ?longitude . ?crimeId ex:hasDate ?date . ?crimeId ex:hasType ?type . ?crimeId ex:hasWeaponInvolved ?weapon . FILTER (regex(str(?state), \"NewYork\")) } LIMIT 100";
+         String query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX ex: <http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#> SELECT (REPLACE(str(?crimeId), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?crimeIdStripped) (REPLACE(str(?county), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?countyStripped) (REPLACE(str(?state), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?stateStripped) (REPLACE(str(?latitude), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?latitudeStripped) (REPLACE(str(?longitude), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?longitudeStripped) ?date (REPLACE(str(?type), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?typeStripped) ?weapon (REPLACE(str(?perp), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?perpStripped) (REPLACE(str(?vic), \"^http://www.semanticweb.org/karthik003/ontologies/2023/10/Project_Team_8#\", \"\") AS ?vicStripped) ?vicGender ?vicAge ?perpGender ?perpAge WHERE { ?crimeId rdf:type owl:NamedIndividual. ?crimeId ex:hasCounty ?county . ?crimeId ex:hasState ?state . ?crimeId ex:hasLatitude ?latitude . ?crimeId ex:hasLongitude ?longitude . ?crimeId ex:hasDate ?date . ?crimeId ex:hasType ?type . ?crimeId ex:hasWeaponInvolved ?weapon . ?crimeId ex:hasPerpetrator ?perp . ?perp ex:hasAge ?perpAge . ?perp ex:hasGender ?perpGender . OPTIONAL { ?crimeId ex:hasVictim ?vic . ?vic ex:hasAge ?vicAge . ?vic ex:hasGender ?vicGender . }";
+        if(state!=null){
+            if(state.equals("ny")){
+                query += " FILTER (regex(str(?state), \"NewYork\")) } LIMIT 100";
+            }else if(state.equals("az")){
+                query += " FILTER (regex(str(?crimeId), \"cra[^#]*$\")) } LIMIT 100";
+            }else if(state.equals("ca")){
+                query += " FILTER (regex(str(?crimeId), \"crc[^#]*$\")) } LIMIT 100";
+            }
+
+        }else{
+            query+=" } LIMIT 10000";
+        }
 
         return query;
     }
